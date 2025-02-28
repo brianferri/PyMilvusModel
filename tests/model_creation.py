740d64d755a6b2f8e93402aa6a9f15850a61465f
Brian Ferri <me@brianferri.dev>
2025-02-28 16:33:59 +0100

    initial commit

---
+++ tests/model_creation.py
@@ -0,0 +1,26 @@
+from typing import List, Optional
+from typing_extensions import Annotated
+from pymilvus import MilvusClient, DataType
+from pymilvusmodel import MilvusIndexParam, MilvusField, MilvusModel
+
+
+class ExampleModel(MilvusModel):
+    indexes: list[MilvusIndexParam] = [
+        MilvusIndexParam("vector", "IVF_FLAT", "vector_index", metric_type="COSINE", params={
+            "nlist": 128
+        })
+    ]
+    id: Annotated[
+        Optional[int],
+        MilvusField(name="id", dtype=DataType.INT64,
+                    is_primary=True, auto_id=True)
+    ] = None
+    vector: Annotated[
+        List[float],
+        MilvusField(name="vector", dtype=DataType.FLOAT_VECTOR, dim=2)
+    ]
+
+
+MILVUS_CLIENT = MilvusClient("http://localhost:19530")
+MilvusModel.metadata.create_all(MILVUS_CLIENT)
+print(MILVUS_CLIENT.list_collections()) # ['ExampleModel']
