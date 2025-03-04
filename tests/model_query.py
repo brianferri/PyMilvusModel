e1b46e9b2a8ba6e8843cc677fd5b4d877319ae0c
Brian Ferri <me@brianferri.dev>
2025-03-04 10:27:07 +0100

    feat: add query test example and update readme

--- tests/model_query.py
+++ tests/model_query.py
@@ -1,20 +1,3 @@
-# PyMilvusModel
-
-Inspired by [SQLModel](https://sqlmodel.tiangolo.com/), PyMilvusModel:
-
-> is a library for interacting with [Milvus](https://milvus.io/) databases from Python code, with Python objects.
-
-and follows the same paradigm, using Python type annotations and powered by [Pydantic](https://docs.pydantic.dev/latest/)
-
-## Installation
-
-```sh
-pip install milvusmodel.py
-```
-
-## Example Usage
-
-```py
 from typing import List, Optional
 from typing_extensions import Annotated
 from pymilvus import MilvusClient, DataType
@@ -40,7 +23,13 @@ class ExampleModel(MilvusModel):
 
 MILVUS_CLIENT = MilvusClient("http://localhost:19530")
 MilvusModel.metadata.create_all(MILVUS_CLIENT)
-print(MILVUS_CLIENT.list_collections()) # ['ExampleModel']
+
 ExampleModel.insert(ExampleModel(vector=[0, 1]))
-print(ExampleModel.query(filter="id>=0")) # [ExampleModel(indexes=[<pymilvusmodel.index.MilvusIndexParam object at 0x105c825d0>], id=456328785400861845, vector=[0.0, 1.0])]
-```
+print(ExampleModel.query(filter="id>=0"))
+"""
+[
+    ...,
+    ExampleModel(indexes=[<pymilvusmodel.index.MilvusIndexParam object at 0x105c825d0>], id=456328785400861845, vector=[0.0, 1.0]),
+    ...
+]
+"""
